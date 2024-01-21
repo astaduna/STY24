f4a86a2edbcc9ec78e0b061abe1b603e5e5ad4f4
asta <astaduna@LAPTOP-EQ4H9L8J>
2024-01-21 23:09:38 +0000

    p2 done

--- Assignment-01/p2/parseint.c
+++ Assignment-01/p2/parseint.c
@@ -6,11 +6,8 @@
 /*
  * Returns the value of c or -1 on error
  */
-int convertDecimalDigit(char c)
-{
-
+int convertDecimalDigit(char c) {
     if (c >= '0' && c <= '9') {
-
 
         return c - '0';
 
@@ -25,9 +22,44 @@ int convertDecimalDigit(char c)
  * Parses a non-negative integer, interpreted as octal when starting with 0,
  * decimal otherwise. Returns -1 on error.
  */
-int parseInt(char *string)
-{
-    (void)string;
+int parseInt(char *string) {
+    if (*string == '\0') {
+        return -1;
+    }
 
-    return -1;
+    int result = 0;
+
+//Check if the sting is octal
+    if (*string == '0') {
+        string ++;
+
+        while (*string >= '0' && *string <= '7') {
+            int decimalValue = convertDecimalDigit(*string);
+            if (decimalValue == -1) {
+                return -1;
+            }
+
+            result = result * 8 + decimalValue;
+            string++;
+        }
+    }
+
+//Check if the string is decimal
+    else if (*string >= '0' && *string <= '9') {
+        while (*string >= '0' && *string <= '9') {
+            int decimalValue = convertDecimalDigit(*string);
+            if (decimalValue == -1) {
+                return -1;
+            }
+
+            result = result * 10 + decimalValue;
+            string++;
+        }
+    }
+    
+    else {
+        return -1;
+    }
+
+    return result;
 }
